65fd04eb28cdeb45e021076605f2c5ea3648326b
Student <example@example.com>
2019-11-20 15:01:44 -0800

    add api functionality

--- src/harvard-service.js
+++ src/harvard-service.js
@@ -1,20 +1,13 @@
 export class HarvardService {
   async getArt(art) {
     try {
-      let response = await fetch(`https://api.harvardartmuseums.org/image?${process.env.API_KEY}&size=3&sort=random&baseimageurl`);
-        .then(function(response) {
-          let jsonifiedResponse = await response.json();
-          return jsonifiedResponse;
-        } catch(error) {
-          console.error("There was an error handling your request: " + error.message);
+      let response = await fetch(`https://api.harvardartmuseums.org/image?apikey=${process.env.API_KEY}&size=3&sort=random&baseimageurl`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch(error) {
+      console.error("There was an error handling your request: " + error.message);
 
 
     }
-   }
- }
-
-
-
-  // .then(function(jsonifiedResponse) {
-  //   getElements(jsonifiedResponse);
-  // });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,17 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { HarvardService } from './harvard-service.js'
+import { HarvardService } from './harvard-service.js';
 
 $(document).ready(function(){
-
+    // event.preventDefault();
   (async () => {
-      let harvardService = new HarvardService();
-      const response = await HarvardService.getArt(art);
-      getElements(response);
-    })();
+    let harvardService = new HarvardService();
+    const response = await harvardService.getArt();
+    getElements(response);
+  })();
 
   function getElements(response) {
-    $('#art1').append(response.records[0].baseimageurl)
+$("#art1").html("<img src='" +  (response.records[0].baseimageurl) +  "'>")
+$("#art2").html("<img src='" +  (response.records[1].baseimageurl) +  "'>")
+$("#art3").html("<img src='" +  (response.records[2].baseimageurl) +  "'>")
+    // $('#art1').text(response.records[0].baseimageurl);
   }
-})
+});
