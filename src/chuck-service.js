f8fe38f786d454842894a5f962d6c08608fc46c4
Student <example@example.com>
2019-11-20 16:41:32 -0800

    safe copy before breaking

---
+++ src/chuck-service.js
@@ -0,0 +1,13 @@
+export class ChuckService {
+  async getChuck(quote) {
+    try {
+      let response = await fetch(`http://api.icndb.com/jokes/random/3`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch(error) {
+      console.error("Chuck doesn't like your request: " + error.message);
+
+
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { HarvardService } from './harvard-service.js';
+import { ChuckService } from './chuck-service.js';
 
 $(document).ready(function(){
     // event.preventDefault();
@@ -12,10 +13,25 @@ $(document).ready(function(){
     getElements(response);
   })();
 
+  (async () => {
+    let chuckService = new ChuckService();
+    const chuckSponse = await chuckService.getChuck();
+    getElements(chuckSponse);
+  })();
+
   function getElements(response) {
 $("#art1").html("<img src='" +  (response.records[0].baseimageurl) +  "'>")
 $("#art2").html("<img src='" +  (response.records[1].baseimageurl) +  "'>")
 $("#art3").html("<img src='" +  (response.records[2].baseimageurl) +  "'>")
+
     // $('#art1').text(response.records[0].baseimageurl);
   }
+
+//   function getElements(chuckSponse) {
+// $("#chuck1").text(response.value[0])
+// $("#art2").html("<img src='" +  (response.records[1].baseimageurl) +  "'>")
+// $("#art3").html("<img src='" +  (response.records[2].baseimageurl) +  "'>")
+//
+//     $('#art1').text(response.records[0].baseimageurl);
+//   }
 });
